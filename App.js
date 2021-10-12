d1de93fa820aca9da256c4be18f0ea15e7974d88
cee <hungns126@gmail.com>
2021-10-12 21:52:19 +0700

    add reactotron

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Login from './src/screens/Login'
 import ProductList from './src/screens/ProductList'
 
 export default function App() {
+  console.tron.log('aaaaa')
+  console.tron.log('bbbb')
   return (
     <View>
       {/* <Login /> */}
